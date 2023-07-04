8e7364a44d548ba201d43880ee4d967dd7a78a34
Michael P. Redlich <mike@redlich.net>
2023-07-04 08:21:42 -0400

    Updates 7/4/2023

---
+++ helidon/config/src/main/java/org/redlich/App.java
@@ -0,0 +1,11 @@
+package org.redlich;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello from the Config component" );
+        }
+    }
